e9d025db542e10f89588387898c3205f0104351c
danielponturo <47391751+danielponturo@users.noreply.github.com>
2025-01-12 20:11:14 +0000

    add initial tests setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,26 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import checker from 'vite-plugin-checker';
 import tailwindcss from 'tailwindcss';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
     plugins: [
         react(),
         checker({
             typescript: true,
         }),
     ],
+    test: {
+        environment: 'jsdom',
+        include: ['**/*.test.tsx'],
+        globals: true,
+        setupFiles: './src/tests/setup.ts',
+    },
     css: {
         postcss: {
             plugins: [tailwindcss()],
         },
     }
-});
+}));
