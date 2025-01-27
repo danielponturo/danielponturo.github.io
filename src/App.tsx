7ac39b1706bf99d538f16682f1ff29f144fed203
danielponturo <47391751+danielponturo@users.noreply.github.com>
2025-01-27 14:59:37 +0000

    remove unnecessary classname

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
     return <>
-        <h1 className='italic'>🚧 Coming soon... 👷</h1>
+        <h1>🚧 Coming soon... 👷</h1>
         <div className='card'>
             <a href='https://github.com/danielponturo'>
                 <button>
